ea25bdb9b065bf924e03586712eeddb12e2b5614
alexandre Girard-Gagnon <p3miki@outlook.com>
2018-04-16 20:40:23 -0400

    Work on sms_fragment_layout

--- app/src/main/java/com/alexandre/crychat/sms/SMSPresenter.java
+++ app/src/main/java/com/alexandre/crychat/sms/SMSPresenter.java
@@ -4,9 +4,17 @@ import android.util.Log;
 
 import com.alexandre.crychat.utilities.CryptoServiceProvider;
 
+import java.io.ByteArrayOutputStream;
+
 public class SMSPresenter implements ISMSContract.Presenter {
 
     private ISMSContract.View frag;
+    /**
+     * Used to identify crypted SMS messages
+     *
+     * Append MD5 to the beginning of a crypted SMS message
+     */
+    private final char[] MD5 = "b746dd1ceef69da6afdcbbaf320e018a".toCharArray();
 
     SMSPresenter(ISMSContract.View fragment)
     {
@@ -24,7 +32,17 @@ public class SMSPresenter implements ISMSContract.Presenter {
         CryptoServiceProvider crypto = new CryptoServiceProvider("test");
         try {
             byte[] crypted = crypto.Encrypt("va chier mon gros calis de chiens de marde");
-            Log.d("CryChat crypted", crypted.toString());
+            byte[] sms = new String(MD5).getBytes();
+
+            /**
+             * :)
+             */
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            output.write(sms);
+            output.write(crypted);
+            byte[] sms_crypted = output.toByteArray();
+
+            Log.d("CryChat crypted", sms_crypted.toString());
             String decrypted = crypto.Decrypt(crypted);
             Log.d("CryChat decrypted", decrypted);
         } catch (Exception e) {
